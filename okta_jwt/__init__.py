ccb2e38c4cc51734e6caf4f0bb364a92fda9f8c0
adithyasampatoor <adithya.sampatoor@gmail.com>
2018-12-19 19:09:20 -0500

    Bump version to 1.3.2

--- okta_jwt/__init__.py
+++ okta_jwt/__init__.py
@@ -1,5 +1,5 @@
 __name__      = 'okta_jwt'
-__version__   = '1.3.1'
+__version__   = '1.3.2'
 __author__    = 'Adithya Sampatoor'
 __license__   = 'MIT'
 __copyright__ = 'Copyright 2018 Adithya Sampatoor'
