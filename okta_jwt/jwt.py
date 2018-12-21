1ebd5f67497a253382c1a35f6fc19f4f661568d4
crouchcd <cameroncrouch36@gmail.com>
2018-12-21 17:14:44 -0500

    rm unused imports, use initial env vars for inttest

--- okta_jwt/jwt.py
+++ okta_jwt/jwt.py
@@ -1,5 +1,3 @@
-import os
-import json
 import requests
 from jose import jwk, jwt
 from requests.auth import HTTPBasicAuth
--- tests/inttest.py
+++ tests/inttest.py
@@ -5,20 +5,17 @@ from okta_jwt import jwt
 
 class IntegrationTest(unittest.TestCase):
 
-    domain          = os.environ['OKTA_DOMAIN']
-    authserver_id   = 'default'
-    authserver_aud  = 'api://default'
-    client_id       = os.environ['OKTA_CLIENT_ID']
+    client_id       = os.environ['OKTA_CLIENT_IDS']
     client_sec      = os.environ['OKTA_CLIENT_SECRET']
-    iss             = 'https://{}/oauth2/{}'.format(domain, authserver_id)
-
+    iss             = os.environ['OKTA_ISSUER']
+    aud             = os.environ['OKTA_AUDIENCE']
     user            = os.environ['OKTA_USERNAME']
     passw           = os.environ['OKTA_PASSWORD']
 
     def test_(self):
         token = jwt.generate_token(self.iss, self.client_id, self.client_sec, self.user, self.passw)
         print(token)
-        validated = jwt.validate_token(token, self.iss, self.authserver_aud, [self.client_id])
+        validated = jwt.validate_token(token, self.iss, self.aud, [self.client_id])
         self.assertTrue(validated)
 
 
